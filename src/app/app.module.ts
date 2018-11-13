02b88dfab8b5cb4701c6362e78dc4a0182046cf6
elbirch2 <hicham.elbirch@gmail.com>
2018-11-13 22:34:46 +0100

    ajout routing de module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {ProduitComponent} from './produit/produit.component';
 import { ProduitMockService } from './produit/produit.mock.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule, appRoutes } from './app.routing.module';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
     NavbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProduitMockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
